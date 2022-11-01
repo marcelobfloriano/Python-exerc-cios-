39b2ad07b19ae43f82395c84a7f3d2fdabab07bf
Marcelo Braga Floriano <113947564+marcelobfloriano@users.noreply.github.com>
2022-11-01 16:14:38 -0300

    dicionarios

--- Exercicios/exer090.py
+++ Exercicios/exer090.py
@@ -1,4 +1,13 @@
-from curses import termattrs
-from ftplib import error_temp
-
-
+alunos = {}
+alunos['nome'] = str(input('Nome do aluno: ')).upper()
+alunos['media'] = float(input('Media do aluno: '))
+if alunos['media'] < 7.5 and alunos['media'] >= 5:
+    alunos['situaçao'] = 'RECUPERAÇAO'
+elif alunos['media'] < 5:
+    alunos['situaçao'] = 'REPROVADO'
+elif alunos['media'] >= 7.5 and alunos['media'] <= 10:
+    alunos['situaçao'] = 'APROVADO'
+print('-=' * 20)
+for k, v in alunos.items():
+    print(f'{k} é igual a {v}')  
+    
